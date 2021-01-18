de6c9cbf7e946ee82f6c9a37cca9fd09c2cea90d
koderoom <koderoom.dev@gmail.com>
2021-01-18 14:11:03 +0530

    init.

---
+++ src/1.calculator.js
@@ -0,0 +1,14 @@
+function addition(n1, n2) {
+  return n1 + n2;
+}
+
+function subtraction(n1, n2) {
+  return n1 - n2;
+}
+
+export function multiplication(n1, n2) {
+  return n1 * n2;
+}
+
+export default addition;
+export { subtraction };
---
+++ src/main.js
@@ -0,0 +1,8 @@
+import addition, { subtraction } from "./1.calculator.js";
+
+function main() {
+  let output = addition(10, 10);
+  console.log(output);
+}
+
+main();
